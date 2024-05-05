d93b1ae364349b9ab4c7606b6c546d73bab1674e
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-05-05 12:46:37 +0400

    First architecture Booking Part

--- main/java/az/edu/turing/StepProject/DAO/BookingDAO.java
+++ main/java/az/edu/turing/StepProject/DAO/BookingDAO.java
@@ -1,4 +1,11 @@
 package az.edu.turing.StepProject.DAO;
 
-public class BookingDAO {
+import java.util.Optional;
+
+public interface BookingDAO<T> {
+    T saveBooking(T booking);
+   Optional<T> getBookingById(String bookingId);
+    T getAllBookings();
+    Optional<T>getBookingsByPassengerName(String name);
+    Optional<T>cancelBooking(String bookingId);
 }
